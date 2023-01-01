b5b6066996380ef0f477f55f0b1ad3c981dd97cf
Wesley De Souza Oliveira <Wesleyxoliveira@gmail.com>
2023-01-01 15:26:48 -0300

    atualizei o curso de javascript

---
+++ modulo_011_javascript_basico/016_for_while/script.js
@@ -0,0 +1,35 @@
+var numero = 5;
+
+/*
+Repetição com for
+
+for(let i = 1; i <= numero; i++) {
+    console.log(i);
+}
+
+console.log("Acabou!");
+
+*/
+
+/*
+// Repetição com while
+while(numero <= 10) {
+    console.log("Número ", numero);
+    numero++;
+}
+
+*/
+
+var teste = Math.random() * 100;
+
+console.log(teste);
+
+while(teste < 90) {
+    console.log(teste);
+    teste = Math.random() * 100;
+}
+
+
+console.log("Acabou!");
+console.log(teste);
+
---
+++ modulo_011_javascript_basico/017_arrays/script.js
@@ -0,0 +1,13 @@
+var alunos = ["Wesley", "José", "Marcos", "Mariana", "Juliana", "Patricia"];
+
+/*
+
+for(let i = 0; i < alunos.length; i++) {
+    console.log(alunos[i]);
+}
+
+*/
+
+for(var i in alunos) {
+    console.log(alunos[i]);
+}
