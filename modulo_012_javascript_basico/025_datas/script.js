d2db4c079e20a7398d35cfe84f02d1436d2a97dc
Wesley De Souza Oliveira <Wesleyxoliveira@gmail.com>
2023-01-06 15:57:24 -0300

    criei aula 025

---
+++ modulo_012_javascript_basico/025_datas/script.js
@@ -0,0 +1,19 @@
+// 1
+var a = new Date();
+console.log(a.getHours() + ":" + a.getMinutes());
+
+console.log("");
+
+//2
+var b = new Date(1991, 02, 11);
+console.log(b);
+
+console.log(b.getFullYear());
+
+console.log("");
+// 3
+
+var c = new Date("mar 11 1991");
+console.log(c);
+
+
