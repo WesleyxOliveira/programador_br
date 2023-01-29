445a74bfe276468cc82ae8680215438a769b8a6b
Wesley De Souza Oliveira <92177435+WesleyxOliveira@users.noreply.github.com>
2023-01-29 16:14:39 -0300

    atualizei o módulo 15

---
+++ modulo_015_html_avancado/004_video2/script.js
@@ -0,0 +1,38 @@
+let video = document.getElementById("video1");
+
+function retroceder() {
+
+
+
+}
+
+function avancar() {
+
+
+
+}
+
+function diminuir_vel() {
+
+
+
+}
+
+function aumentar_vel() {
+
+
+
+}
+
+function play() {
+
+    video.play();
+
+}
+
+function stop() {
+
+    video.pause();
+    video.CurrentTime = 0;
+
+}
