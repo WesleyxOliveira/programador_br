3d4febe57642b59a4d50929122b6f026c9ee5284
Wesley De Souza Oliveira <92177435+WesleyxOliveira@users.noreply.github.com>
2023-01-29 18:57:25 -0300

    atualizei

--- modulo_015_html_avancado/004_video2/script.js
+++ modulo_015_html_avancado/004_video2/script.js
@@ -1,27 +1,19 @@
 let video = document.getElementById("video1");
 
 function retroceder() {
-
-
-
+    video.currentTime -= 15;
 }
 
 function avancar() {
-
-
-
+    video.currentTime += 15;
 }
 
 function diminuir_vel() {
-
-
-
+    video.playbackRate -= 0.1;
 }
 
 function aumentar_vel() {
-
-
-
+    video.playbackRate += 0.1;
 }
 
 function play() {
@@ -30,9 +22,13 @@ function play() {
 
 }
 
-function stop() {
+function pause() {
+    video.pause();
+}
+
+function parar() {
 
     video.pause();
-    video.CurrentTime = 0;
-
+    video.currentTime = 0;
+    video.poster = "./assets/imagens/poster.jpg";
 }
