f3fe17355e9226703fd50ac5ea5b08c7a7c2ac8d
Wesley De Souza Oliveira <92177435+WesleyxOliveira@users.noreply.github.com>
2023-03-22 22:29:12 -0300

    atualizei a seção de js avançado

---
+++ modulo_018_js-avancado/006_async-await/script.js
@@ -0,0 +1,33 @@
+let usuarios = ["Adriano", "Márcia", "José"];
+
+function inserirUsuario(nome) {
+
+    let promise = new Promise(function(resolve, reject) {
+
+        setTimeout(() => {
+            usuarios.push(nome);
+
+            let error = false;
+
+            if(!error) {
+                resolve()
+            }else {
+                reject({msg: "Mensagem de erro de qualquer coisa"});
+            }
+        }, 1000);
+    });
+
+    return promise;
+}
+
+function listarUsuarios() {
+    console.log(usuarios);
+}
+
+function executar() {
+    inserirUsuario("Wesley");
+    listarUsuarios();
+}
+
+executar();
+
