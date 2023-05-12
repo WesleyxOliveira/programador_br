b56980b82f7ff1c8978d2fe6217e445aa8814ba9
Wesley De Souza Oliveira <92177435+WesleyxOliveira@users.noreply.github.com>
2023-05-11 23:31:12 -0300

    Jogo da memoria
    Criei a lógica de testar se o jogo forma par e sem teve vencedor

--- modulo_021_jogo-da-memoria/assets/scripts/game.js
+++ modulo_021_jogo-da-memoria/assets/scripts/game.js
@@ -1,33 +1,8 @@
 let game = {
+
     lockMode: false,
     firstCard: null,
     secondCard: null,
-    setCard: function (id) {
-        let card = this.cards.filter(card => card.id === id)[0];
-
-        if(card.flipped || this.lockMode) {
-            return false;
-        }
-
-        if(!this.firstCard) {
-            this.firstCard = card;
-            return true;
-        } else {
-            this.secondCard = card;
-            this.lockMode = true;
-            return true;
-        }
-    },
-
-    checkMatch: function() {
-        return this.firstCard.icon === this.secondCard.icon;
-    },
-
-    clearCards: function() {
-        this.firstCard = null;
-        this.secondCard = null;
-        this.lockMode = false;
-    },
 
     techs: [
         'bootstrap',
@@ -41,7 +16,49 @@ let game = {
         'node',
         'react'
     ],
+
     cards: null,
+
+    setCard: function (id) {
+        let card = this.cards.filter(card => card.id === id)[0];
+        if(card.flipped || this.lockMode) {
+            return false;
+        }
+
+        if(!this.firstCard) {
+            this.firstCard = card;
+            this.firstCard.flipped = true;
+            return true;
+        } else {
+            this.secondCard = card;
+            this.secondCard.flipped = true;
+            this.lockMode = true;
+            return true;
+        }
+    },
+
+    checkMatch: function() {
+        if(!this.firstCard || !this.secondCard) {
+            return false
+        }
+        return this.firstCard.icon === this.secondCard.icon;
+    },
+
+    clearCards: function() {
+        this.firstCard = null;
+        this.secondCard = null;
+        this.lockMode = false;
+    },
+
+    unflipCards() {
+        this.firstCard.flipped = false;
+        this.secondCard.flipped = false;
+        this.clearCards();
+    },
+
+    checkGameOver() {
+        return this.cards.filter(card => !card.flipped).length == 0;
+    },
 
     // createCardsFromTechs(techs);
     createCardsFromTechs: function (techs) {
@@ -72,7 +89,7 @@ let game = {
 
     createIdWithTech: function (tech) {
         return tech + parseInt(Math.random() * 1000);
-    }, 
+    },
 
     shuffleCards: function (cards) {
         let currentIndex = this.cards.length;
