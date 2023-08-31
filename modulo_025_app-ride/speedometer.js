d71f762c6377d0d1472749079db005907b8b1aa7
Wesley De Souza Oliveira <92177435+WesleyxOliveira@users.noreply.github.com>
2023-08-30 22:33:38 -0300

    atualizei o projeto ride

--- modulo_025_app-ride/speedometer.js
+++ modulo_025_app-ride/speedometer.js
@@ -3,36 +3,27 @@ const startBtn = document.querySelector('#start');
 const stopBtn = document.querySelector('#stop');
 
 let watchId = null;
+let currentRide = null;
 
 startBtn.addEventListener('click', () => {
-    if(watchId) 
-        return
-
 
     function handleSuccess(position) {
-        speedElement.innerHTML = position.coords.speed ? (position.coords.speed * 3.6).toFixed(1) : 0;
+        speedElement.innerText = position.coords.speed ? (position.coords.speed * 3.6).toFixed(1) : 0;
     }
-    
+
     function handleError(error) {
         console.log(error.msg);
     }
 
-    const options = {enableHighAccuracy:true}
+    const options = {enableHighAccuracy: true};
 
-    watchId = navigator.geolocation.watchPosition(handleSuccess, handleError, options);
+    navigator.geolocation.watchPosition(handleSuccess, handleError, options);
 
-    startBtn.classList.add('d-none')
+    startBtn.classList.add('d-none');
     stopBtn.classList.remove('d-none');
 });
 
 stopBtn.addEventListener('click', () => {
-    if(!watchId)
-        return;
-
-    navigator.geolocation.clearWatch(watchId);
-    watchId = null;
     stopBtn.classList.add('d-none');
     startBtn.classList.remove('d-none');
 })
-
-console.log(stopBtn);
