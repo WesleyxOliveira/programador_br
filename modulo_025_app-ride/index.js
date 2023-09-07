e49391acb18cc4bc9adf38da2df2e0e8a94afb13
devxiz <wesley@gmail.com>
2023-09-07 11:45:23 -0300

    atualizei

--- modulo_025_app-ride/index.js
+++ modulo_025_app-ride/index.js
@@ -1,32 +1,26 @@
-const rideListElement = document.querySelector('#rideList');
+const rideListElement = document.querySelector("#rideList");
+
 const allRides = getAllRides();
 
 allRides.forEach(async ([id, value]) => {
     const ride = JSON.parse(value);
     ride.id = id;
-    // console.log(ride);
-
 
     const firstPosition = ride.data[0];
-    const firstLocationData = await getLocationData(firstPosition.latitude, firstPosition.longitude); 
+    const firstLocationData = await getLocationData(firstPosition.latitude, firstPosition.longitude);
+
+    // console.log(locationData);
 
     const itemElement = document.createElement('li');
     itemElement.id = ride.id;
+    itemElement.innerText = `${firstLocationData.city} - ${firstLocationData.countryCode}`
 
-    const cityDiv = document.createElement('div');
-    cityDiv.innerText = `${firstLocationData.city} - ${firstLocationData.countryCode}`;
-
-    itemElement.appendChild(cityDiv);
     rideListElement.appendChild(itemElement);
 })
 
 async function getLocationData(latitude, longitude) {
-    const url = `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}&=localityLanguage=en`
+    const url = `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}&=localityLanguage=en`;
 
     const response = await fetch(url)
-    return await response.json()
-}
-
-function getMaxSpeed() {
-    
+    return await response.json();
 }
