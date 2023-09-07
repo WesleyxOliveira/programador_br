b609f89b04c32e7fd729d3fbd49e16dbade2093f
devxiz <wesley@gmail.com>
2023-09-07 15:24:11 -0300

    atualizei a seção 025
    Finalizei a função que pega a velocidade máxima

--- modulo_025_app-ride/index.js
+++ modulo_025_app-ride/index.js
@@ -6,15 +6,22 @@ allRides.forEach(async ([id, value]) => {
     const ride = JSON.parse(value);
     ride.id = id;
 
+    
+
     const firstPosition = ride.data[0];
     const firstLocationData = await getLocationData(firstPosition.latitude, firstPosition.longitude);
-
-    // console.log(locationData);
-
     const itemElement = document.createElement('li');
-    itemElement.id = ride.id;
-    itemElement.innerText = `${firstLocationData.city} - ${firstLocationData.countryCode}`
 
+    itemElement.id = ride.id;
+    const cityDiv = document.createElement('div');
+
+    cityDiv.innerText = `${firstLocationData.city} - ${firstLocationData.countryCode}`;
+
+    const maxSpeedDiv = document.createElement('div');
+    maxSpeedDiv.innerText = getMaxSpeed(ride.data);
+
+    itemElement.appendChild(cityDiv);
+    itemElement.appendChild(maxSpeedDiv);
     rideListElement.appendChild(itemElement);
 })
 
@@ -23,4 +30,15 @@ async function getLocationData(latitude, longitude) {
 
     const response = await fetch(url)
     return await response.json();
+}
+
+function getMaxSpeed(positions) {
+    let maxSpeed = 0;
+    positions.forEach(position => {
+        if(position.speed != null && position.speed > maxSpeed) {
+            maxSpeed = position.speed;
+        }
+    })
+
+    return (maxSpeed * 3.6).toFixed(1);
 }
