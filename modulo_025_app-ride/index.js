bd30094b730903cd5970183dc1e83555dbf1e8f7
devxiz <wesleyxoliveira@gmail.com>
2023-09-18 22:16:23 -0300

    Criei a tela de detalhes

--- modulo_025_app-ride/index.js
+++ modulo_025_app-ride/index.js
@@ -11,6 +11,10 @@ allRides.forEach(async ([id, value]) => {
     itemElement.className = 'd-flex p-1 align-items-center justify-content-between shadow-sm gap-3';
     rideListElement.appendChild(itemElement);
 
+    itemElement.addEventListener('click', () => {
+        window.location.href = `./detail.html?id=${ride.id}`;
+    })
+
 
     const firstPosition = ride.data[0];
     const firstLocationData = await getLocationData(firstPosition.latitude, firstPosition.longitude);
@@ -52,73 +56,73 @@ allRides.forEach(async ([id, value]) => {
     itemElement.appendChild(dataElement);    
 })
 
-async function getLocationData(latitude, longitude) {
-    const url = `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}&=localityLanguage=en`;
+// async function getLocationData(latitude, longitude) {
+//     const url = `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}&=localityLanguage=en`;
 
-    const response = await fetch(url)
-    return await response.json();
-}
+//     const response = await fetch(url)
+//     return await response.json();
+// }
 
-function getMaxSpeed(positions) {
-    let maxSpeed = 0;
-    positions.forEach(position => {
-        if(position.speed != null && position.speed > maxSpeed) {
-            maxSpeed = position.speed;
-        }
-    })
+// function getMaxSpeed(positions) {
+//     let maxSpeed = 0;
+//     positions.forEach(position => {
+//         if(position.speed != null && position.speed > maxSpeed) {
+//             maxSpeed = position.speed;
+//         }
+//     })
 
-    return (maxSpeed * 3.6).toFixed(1);
-}
+//     return (maxSpeed * 3.6).toFixed(1);
+// }
 
-function getDistance(position) {
-    const earthRadiusKm = 6371;
-    let totalDistance = 0;
-    for(let i = 0; i < position.length -1; i++) {
-        const p1 = {latitude:position[i].latitude, longitude: position[i].longitude}
+// function getDistance(position) {
+//     const earthRadiusKm = 6371;
+//     let totalDistance = 0;
+//     for(let i = 0; i < position.length -1; i++) {
+//         const p1 = {latitude:position[i].latitude, longitude: position[i].longitude}
         
-        const p2 = {latitude:position[i + 1].latitude, longitude: position[i + 1].longitude}
+//         const p2 = {latitude:position[i + 1].latitude, longitude: position[i + 1].longitude}
 
-        const deltaLatitude = toRad(p2.latitude - p1.latitude);
-        const deltaLongitude = toRad(p2.longitude - p1.longitude);
+//         const deltaLatitude = toRad(p2.latitude - p1.latitude);
+//         const deltaLongitude = toRad(p2.longitude - p1.longitude);
 
-        const a = Math.sin(deltaLatitude/2) * Math.sin(deltaLatitude/2) + Math.sin(deltaLongitude/2) * Math.sin(deltaLongitude/2) * Math.cos(toRad(p1.latitude)) * Math.cos(toRad(p2.latitude))
+//         const a = Math.sin(deltaLatitude/2) * Math.sin(deltaLatitude/2) + Math.sin(deltaLongitude/2) * Math.sin(deltaLongitude/2) * Math.cos(toRad(p1.latitude)) * Math.cos(toRad(p2.latitude))
 
-        const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+//         const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
 
-        const distance = earthRadiusKm * c
+//         const distance = earthRadiusKm * c
 
-        totalDistance += distance;
-    }
+//         totalDistance += distance;
+//     }
 
-    function toRad(degree) {
-        return degree * Math.PI / 180
-    }
+//     function toRad(degree) {
+//         return degree * Math.PI / 180
+//     }
 
-    return totalDistance.toFixed(2);
-}
+//     return totalDistance.toFixed(2);
+// }
 
-function getDuration(ride) {
-    function format(number, digits) {
-        return String(number.toFixed(0).padStart(2, '0'));
-    }
+// function getDuration(ride) {
+//     function format(number, digits) {
+//         return String(number.toFixed(0).padStart(2, '0'));
+//     }
 
-    const interval = (ride.stopTime - ride.startTime)/1000;
+//     const interval = (ride.stopTime - ride.startTime)/1000;
 
-    const minutes = Math.trunc(interval / 60);
-    const seconds = interval % 60;
+//     const minutes = Math.trunc(interval / 60);
+//     const seconds = interval % 60;
 
-    return `${format(minutes, 2)}:${format(seconds, 2)}`;
-}
+//     return `${format(minutes, 2)}:${format(seconds, 2)}`;
+// }
 
-function getStartDate(ride) {
-    const d = new Date(ride.startTime);
+// function getStartDate(ride) {
+//     const d = new Date(ride.startTime);
 
-    const day = d.toLocaleString(`en-US`, {day: 'numeric'});
-    const month = d.toLocaleString(`en-US`, {month: 'short'});
-    const year = d.toLocaleString(`en-US`, {year: 'numeric'});
+//     const day = d.toLocaleString(`en-US`, {day: 'numeric'});
+//     const month = d.toLocaleString(`en-US`, {month: 'short'});
+//     const year = d.toLocaleString(`en-US`, {year: 'numeric'});
 
-    const hour = d.toLocaleString(`en-US`, {hour: '2-digit', hour12:false});
-    const minute = d.toLocaleString(`en-US`, {minute: '2-digit'});
+//     const hour = d.toLocaleString(`en-US`, {hour: '2-digit', hour12:false});
+//     const minute = d.toLocaleString(`en-US`, {minute: '2-digit'});
 
-    return `${hour}:${minute} - ${month} ${day} ${year}`;
-}
+//     return `${hour}:${minute} - ${month} ${day} ${year}`;
+// }
