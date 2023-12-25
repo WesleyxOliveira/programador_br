b0685dc030418b49358090a4a770088f965f50fb
wesleyXiz <wesleyxoliveira@gmail.com>
2023-12-25 20:18:44 -0300

    Iniciei a seção de firebase store

---
+++ modulo_027_firebase-storage/001_referencias/js/script.js
@@ -0,0 +1,28 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyASXEa5HsKvtmS8T08yMvXw-TMv0Iac7g4",
+    authDomain: "cursodevweb-7c15b.firebaseapp.com",
+    projectId: "cursodevweb-7c15b",
+    storageBucket: "cursodevweb-7c15b.appspot.com",
+    messagingSenderId: "679842901906",
+    appId: "1:679842901906:web:84a61bda95911b86d85b4b"
+  };
+  
+  firebase.initializeApp(firebaseConfig);
+
+
+  const storage = firebase.storage();
+
+  /* Criando referência para pasta raiz 
+
+  const ref = storage.ref();
+
+  */
+
+  /* Cria referência para a pasta images que esta dentro da pasta raiz */
+  const ref = storage.ref();
+
+  ref.listAll().then(res => {
+    res.items[0].getDownloadURL().then(url => {
+        console.log(url);
+    });
+  })
