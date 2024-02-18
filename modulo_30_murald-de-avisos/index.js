bb8b5a06033868d51ef41b4a81986f58c4522a7c
Wesley <wesleyxoliveira@gmail.com>
2024-02-18 19:07:09 -0300

    Separei as funçoes

--- modulo_30_murald-de-avisos/index.js
+++ modulo_30_murald-de-avisos/index.js
@@ -2,43 +2,22 @@ const PORT = 3000;
 const express = require('express');
 const app = express();
 app.use(express.json());
-
-let posts = [
-    {
-        id: 'klasfakls',
-        title: 'Teste do mural',
-        description: 'Descrição de teste'
-    },
-    {
-        id: 'klasfakls',
-        title: 'Teste do mural',
-        description: 'Descrição de teste'
-    },
-    {
-        id: 'klasfakls',
-        title: 'Teste do mural',
-        description: 'Descrição de teste'
-    }
-];
+const posts = require('./model/model');
 
 app.get('/all', (req, res) => {
-    res.json(JSON.stringify(posts));
+    res.json(JSON.stringify(posts.getAll()));
 });
 
 
 app.post('/new', (req, res) => {
-    let id = generateId();
     let title = req.body.title;
     let description = req.body.description;
 
-    posts.push({id, title, description});
-    res.send('Post adicionado!');
+    posts.newPost(title, description);
+    res.send('Post adicionado com sucesso!');
 });
 
 app.listen(PORT, () => {
     console.log('Server running on port:', PORT);
 });
 
-function generateId() {
-    return Math.random().toString(36).substring(2, 9);
-}
---
+++ modulo_30_murald-de-avisos/model/model.js
@@ -0,0 +1,23 @@
+
+
+module.exports = {
+    posts: [
+        {
+            id: 'ljasdfj',
+            title: 'Teste de Mural',
+            description: 'Descrição teste'
+        }
+    ],
+
+    getAll() {
+        return this.posts;
+    },
+
+    newPost(title, description) {
+        this.posts.push({id: generateId(), title, description});
+    }
+}
+
+function generateId() {
+    return Math.random().toString(36).substring(2, 9);
+}
