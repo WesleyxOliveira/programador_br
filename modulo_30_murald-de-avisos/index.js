8965019d1f52bd626edab6400a00bf7df547a740
Wesley <wesleyxoliveira@gmail.com>
2024-02-20 21:18:30 -0300

    Separei as rotas

--- modulo_30_murald-de-avisos/index.js
+++ modulo_30_murald-de-avisos/index.js
@@ -1,21 +1,14 @@
 const PORT = 3000;
 const express = require('express');
 const app = express();
-app.use(express.json());
-const posts = require('./model/model');
-
-app.get('/all', (req, res) => {
-    res.json(JSON.stringify(posts.getAll()));
-});
+const path = require('path');
+const apiRoutes = require('./routes/api');
 
 
-app.post('/new', (req, res) => {
-    let title = req.body.title;
-    let description = req.body.description;
+app.use('/api', apiRoutes);
 
-    posts.newPost(title, description);
-    res.send('Post adicionado com sucesso!');
-});
+app.use('/', express.static(path.join(__dirname, 'public')));
+
 
 app.listen(PORT, () => {
     console.log('Server running on port:', PORT);
