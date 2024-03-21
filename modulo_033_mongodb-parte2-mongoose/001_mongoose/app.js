5bb4ac11f4b5e0b18359e32a0a3e774144999689
Wesley <wesleyxoliveira@gmail.com>
2024-03-20 21:03:18 -0300

    Aprendi um puco mais sobre como conectar o mongoose ao MongoDb

--- modulo_033_mongodb-parte2-mongoose/001_mongoose/app.js
+++ modulo_033_mongodb-parte2-mongoose/001_mongoose/app.js
@@ -3,12 +3,32 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 
-//Conectando o mongoose ao MongoDb e ao bando de dados (links)
-mongoose.connect('mongodb://localhost/links')
+/*
+Conectando o mongoose ao MongoDb e ao bando de dados (blog) através do then
+mongoose.connect('mongodb://localhost/blog')
+    .then(db => {
+        console.log(db);
+    }).catch(error => {
+        console.log(error);
+    })
 
-let db = mongoose.connection;
+*/
 
-console.log(db);
+
+
+
+//Esta é uma outra forma de fazer, e usa uma função assíncrona 
+mongoose.connect('mongodb://localhost/blog');
+
+const db = mongoose.connection;
+
+//É adicionado um listener ao evento db.on("error") para que quando houver um erro esse código execute, é usado listener, pois o mongoose.connection é assyncrono.
+db.on('error', () => { console.log('houve um error')});
+
+
+    
+//db.once('open') é para executar apenas uma vez quando o banco de dados for carregado.
+db.once('open', () => {console.log('Banco carregado...')});
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
