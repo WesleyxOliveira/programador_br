52e4f7a20bce20ed5307914e0cb02220b1a02c96
Wesley <wesleyxoliveira@gmail.com>
2024-07-25 21:47:46 -0300

    Criei a rota e a função para deletar um documento

---
+++ modulo_033_mongodb-parte2-mongoose/013_apagando_um_documento/routes/linkRoute.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+const linkController = require('../controllers/linkController');
+
+router.get('/', (req, res)=> {res.render('index',{error: false, body: {}, links})});
+
+router.get('/all', linkController.allLinks);
+
+router.get('/:title', linkController.redirect);
+
+router.post('/', express.urlencoded({extended: true}), linkController.addLink);
+
+router.delete('/:id', linkController.deleteLink);
+router.delete('/', express.urlencoded({extended: true}), linkController.deleteLink);
+
+module.exports = router;
