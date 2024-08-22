d2325b7b364de99aaf339d9dcd6f33d4a5a068c5
Wesley <wesleyxoliveira@gmail.com>
2024-08-21 21:05:53 -0300

    criei um usuáro no banco de dados mongo db atlas

---
+++ modulo_034_login_jwt/005_criando_usuario/controllers/userController.js
@@ -0,0 +1,27 @@
+const User = require('../models/User');
+
+
+const userController = {
+    register: async function (req, res) {
+        
+        const user = new User({
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password
+        });
+
+        try {
+            const savedUser = await user.save();
+            res.send(savedUser);
+        } catch (error) {
+            res.status(400).send(error);
+        }
+    }, 
+
+    login (req, res) {
+        console.log('Login');
+        res.send('Login')
+    }
+}
+
+module.exports = userController;
