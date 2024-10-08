c01154e7145dd6dd3fba21b5396a22ff6a4ba011
Wesley <wesleyxoliveira@gmail.com>
2024-10-08 20:53:09 -0300

    Aprendi a conectar o front com o back com o socket.io

---
+++ modulo_036_socket/002_conectando_o_front_com_o_back/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const http = require('http');
+const socketIO = require('socket.io');
+
+app.use('/', express.static(path.join(__dirname, 'public')));
+
+const server = app.listen(3000, ()=> {
+    console.log('Server Running...');
+});
+
+const io = socketIO(server);
+
+io.on('connection', (socket)=> {
+    console.log('New connection.');
+})
